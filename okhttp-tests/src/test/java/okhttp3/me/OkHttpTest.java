fb4e31f3894854e0a1a00a51fd46fbbd4d1060f0
sly <853583852@qq.com>
2023-04-17 23:13:32 +0800

    my branch

--- okhttp-tests/src/test/java/okhttp3/me/OkHttpTest.java
+++ okhttp-tests/src/test/java/okhttp3/me/OkHttpTest.java
@@ -1,7 +1,9 @@
 package okhttp3.me;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,7 +16,7 @@ import java.io.IOException;
  * @date: 2023/4/4 17:57
  */
 public class OkHttpTest {
-
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     @Test
     public void okHTTP() throws IOException {
         OkHttpClient okHttpClient = new OkHttpClient();
@@ -22,5 +24,20 @@ public class OkHttpTest {
         Request req = builder.url("https://www.baidu.com").build();
         String s = okHttpClient.newCall(req).execute().body().source().readUtf8();
         Assert.assertTrue(StringUtils.isNotBlank(s));
+    }
+
+    @Test
+    public void postRequest() {
+        String content = "{\n"
+                + "    \"masterDesignCodeList\": [\n"
+                + "        \"100\"\n"
+                + "    ],\n"
+                + "    \"masterDesignCode\": \"MD1643196796614213632\"\n"
+                + "}";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(JSON, content);
+
+
+
     }
 }
